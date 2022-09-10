d1955c41e2021c54ad38da47a2756b7cc46bf635
Evandro Pomatti <evandro.pomatti@gmail.com>
2022-09-10 18:09:12 -0300

    express app use json

--- worker/src/worker.ts
+++ worker/src/worker.ts
@@ -2,18 +2,18 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 
+const port: number = Number(process.env.PORT);
 const LONG_RUNNING_TASK_DURATION: number = Number(process.env.LONG_RUNNING_TASK_DURATION);
 console.log(`Long running task will delay for: ${LONG_RUNNING_TASK_DURATION} ms`);
+
+const app = express();
+app.use(express.json());
 
 const delay = async (ms: number) => {
   return await new Promise(resolve => setTimeout(resolve, ms));
 }
 
 const start = async () => {
-
-  const app = express()
-  const port: number = Number(process.env.PORT);
-
   app.post('/', async (req, res) => {
     console.log("received message");
     console.log(req.body);
