1113ee4d444381619a42a69d25ac51046202601e
Evandro Pomatti <evandro.pomatti@gmail.com>
2022-09-10 10:34:18 -0300

    cleanup

--- worker/src/main.ts
+++ worker/src/main.ts
@@ -1,10 +1,8 @@
-import { start } from './processor'
 import * as worker from './worker'
 
 (async () => {
   try {
     await worker.start();
-    // await start();
   } catch (e) {
     console.error(e);
   }
