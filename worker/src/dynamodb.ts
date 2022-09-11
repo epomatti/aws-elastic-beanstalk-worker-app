bc465f12033b5eb75bc6c8b7ada4a77fcfd282e3
Evandro Pomatti <evandro.pomatti@gmail.com>
2022-09-11 09:03:21 -0300

    arapuca

--- worker/src/dynamodb.ts
+++ worker/src/dynamodb.ts
@@ -4,8 +4,10 @@ const region = process.env.DYNAMODB_REGION;
 const tableName = process.env.DYNAMODB_TABLE_NAME;
 
 export const putItem = async (id: string, status: string) => {
+  console.log("1");
   const client = new DynamoDBClient({ region: region });
-
+  console.log("2");
+  
   const input: PutItemCommandInput = {
     TableName: tableName,
     Item: {
@@ -13,8 +15,11 @@ export const putItem = async (id: string, status: string) => {
       "Status": { "S": status }
     }
   }
-
+  console.log("3");
+  
   const command = new PutItemCommand(input);
+  console.log("4");
   const output = await client.send(command);
+  console.log("5");
   console.log(output);
 }
